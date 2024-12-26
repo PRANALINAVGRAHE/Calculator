bd8d473cae51f70c7bcfacd87036d2c6242ad7a9
PRANALINAVGRAHE <navgrahepranali@gmail.com>
2024-12-26 16:21:50 +0530

    Adding all my files

---
+++ App.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { useState } from 'react';
+import { SafeAreaView, StyleSheet, Switch, Text, View } from 'react-native';
+import { myColors } from './src/styles/Colors';
+import { ThemeContext } from './src/context/ThemeContext';
+import MyKeyboard from './src/components/MyKeyboard';
+
+export default function App() {
+  const [theme, setTheme] = useState('light');
+  return (
+    <ThemeContext.Provider value={theme}>
+      <SafeAreaView style={theme === 'light' ? styles.container : [styles.container, { backgroundColor: 'black' }]}>
+        <View style={styles.switchContainer}>
+          <Switch
+            value={theme === 'dark'}
+            onValueChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+          />
+        </View>
+        <MyKeyboard />
+        <View style={styles.footer}>
+          <Text style={theme === 'light' ? styles.footerTextLight : styles.footerTextDark}>
+            <b>Calc by Pranali</b>
+          </Text>
+        </View>
+      </SafeAreaView>
+    </ThemeContext.Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: myColors.light,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  switchContainer: {
+    marginTop: 40, // Adjust this value to control how far down the toggle button appears
+    alignItems: 'center',
+  },
+  footer: {
+    position: 'absolute',
+    bottom: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+  },
+  footerTextLight: {
+    color: 'black',
+    fontSize: 16,
+  },
+  footerTextDark: {
+    color: 'white',
+    fontSize: 16,
+  },
+});
